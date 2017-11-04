b4d6fc25f8b1fa0898456f5cf3dd3b93c2bd0b84
mahesaiqbal <iqbalridwansyah.ir@gmail.com>
2017-11-04 12:00:11 +0700

    Perbaikan Fitur

--- app/src/main/java/com/mahesaiqbal/darkskyclient/MainActivity.java
+++ app/src/main/java/com/mahesaiqbal/darkskyclient/MainActivity.java
@@ -11,6 +11,7 @@ import com.mahesaiqbal.darkskyclient.events.ErrorEvent;
 import com.mahesaiqbal.darkskyclient.events.WeatherEvent;
 import com.mahesaiqbal.darkskyclient.services.WeatherService;
 import com.mahesaiqbal.darkskyclient.services.WeatherServiceProvider;
+import com.mahesaiqbal.darkskyclient.util.WeatherIconUtil;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -76,20 +77,7 @@ public class MainActivity extends AppCompatActivity {
         tempTextView.setText(String.valueOf(Math.round(currently.getTemperature())));
         summaryTextView.setText(currently.getSummary());
 
-        Map<String, Integer> iconMap = new HashMap<>();
-        iconMap.put("clear-day", R.drawable.ic_clear_day);
-        iconMap.put("clear-night", R.drawable.ic_clear_night);
-        iconMap.put("rain", R.drawable.ic_rain);
-        iconMap.put("snow", R.drawable.ic_snow);
-        iconMap.put("sleet", R.drawable.ic_sleet);
-        iconMap.put("wind", R.drawable.ic_wind);
-        iconMap.put("fog", R.drawable.ic_fog);
-        iconMap.put("cloudy", R.drawable.ic_cloudy);
-        iconMap.put("partly-cloudy-day", R.drawable.ic_partly_cloudy_day);
-        iconMap.put("partly-cloudy-night", R.drawable.ic_partly_cloudy_night);
-        iconMap.put("thunderstorm", R.drawable.ic_thunderstorm);
-
-        iconImageView.setImageResource(iconMap.get(currently.getIcon()));
+        iconImageView.setImageResource(WeatherIconUtil.ICONS.get(currently.getIcon()));
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
